fe7d87ece696425574e7790c85aaa354ea2d9278
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2019-10-03 20:15:31 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,21 +10,20 @@ variable "mypublic-key" {
 
 variable "gcp_region" {
   description = "GCP region, e.g. us-east1"
-  default = "us-west1"
+  default     = "us-west1"
 }
 
 variable "gcp_zone" {
   description = "GCP zone, e.g. us-east1-b (which must be in gcp_region)"
-  default = "us-west1-b"
+  default     = "us-west1-b"
 }
 
 variable "gcp_project" {
   description = "GCP project name"
-  default="dohoney-gcp-demo"
+  default     = "dohoneydemos"
 }
 
 variable "gcp_credentials" {
   description = "GCP project credentials"
-  default="/Users/johndohoneyjr/.ssh/terraform.json"
+  default     = "/Users/johndohoney/.config/gcloud/dohoneydemos-40ad4f4d925a.json"
 }
-
